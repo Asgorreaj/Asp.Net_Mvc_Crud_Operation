a84769c2a1192eee9ebfb7a6a1f85d428dc8cd9f
MD. Asgor Hossain Reaj <asgorreaj@gmail.com>
2023-10-23 02:11:07 +0600

    ADD DELETE CREATE EDIT DETAILS

---
+++ WebApplication12/Controllers/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using Antlr.Runtime.Misc;
+using Microsoft.Ajax.Utilities;
+using System;
+using WebApplication12.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication12.EF;
+
+namespace WebApplication12.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult showAllDetails()
+        {
+            var db = new trydborderEntities();
+            var data = db.Products.ToList();
+
+            return View(data);
+        }
+
+        public ActionResult showAllDetailsEdit()
+        {
+            var db = new trydborderEntities();
+            var data = db.Products.ToList();
+
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+            
+        }
+        [HttpPost]
+        public ActionResult Create(Product p) { 
+            var db  = new trydborderEntities();
+            db.Products.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("showAllDetails");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new trydborderEntities();
+            var ex = (from p in db.Products
+                      where p.product_id == id 
+                      select p).SingleOrDefault();
+            return View(ex);
+            
+        }
+        [HttpPost]
+        public ActionResult Edit(Product p) { 
+            var db  = new trydborderEntities();
+            var exdata = db.Products.Find(p.product_id);
+            exdata.product_name = p.product_name;
+            exdata.price = p.price;
+            db.SaveChanges();
+            return RedirectToAction("showAllDetails");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var db = new trydborderEntities();
+            var exdata = db.Products.Find(id);
+            return View(exdata);
+
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, string action)
+        {
+            if (action == "Delete")
+            {
+                var db = new trydborderEntities();
+                var exdata = db.Products.Find(id);
+                db.Products.Remove(exdata);
+                db.SaveChanges();
+                return RedirectToAction("showAllDetails");
+            }
+            else
+            {
+                return RedirectToAction("showAllDetails");
+            }
+        }
+
+       
+
+
+
+
+
+
+
+
+    }
+}
---
+++ WebApplication12/Models/Cart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication12.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int CartId { get; set; }
+
+        public List<CartItem> Items { get; set; }
+    }
+
+    public class CartItem
+    {
+        [Key]
+        public int CartItemId { get; set; }
+
+        [Required]
+        public Food FoodItem { get; set; }
+
+        public int Quantity { get; set; }
+    }
+
+    public class Food
+    {
+        [Key]
+        public int FoodId { get; set; }
+        [Required]
+        public string FoodName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public float Price { get; set; }
+    }
+}
